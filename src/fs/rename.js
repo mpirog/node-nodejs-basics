eea70df1c705083c5534ccaa301fb9682818ceed
imychko <imychko@lumentouch.com>
2022-06-02 22:48:08 +0300

    feat: implement rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,14 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { __dirname } from './custom-module.js';
+
 export const rename = async () => {
-    // Write your code here 
+  const wrongFilePath = path.resolve(__dirname, './files/wrongFilename.txt');
+  const properFilePath = path.resolve(__dirname, './files/properFilename.md');
+
+  try {
+    await fs.rename(wrongFilePath, properFilePath);
+  } catch {
+    throw new Error('FS operation failed')
+  }
 };
