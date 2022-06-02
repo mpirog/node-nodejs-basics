2659e21c3d9e92c01f3e1dcdef299260bcaf45f5
imychko <imychko@lumentouch.com>
2022-06-02 20:22:16 +0300

    feat: implement create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,30 @@
+import fs from 'fs/promises';
+import path from 'path';
+
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export const create = async () => {
-    // Write your code here 
+  const data = 'I am fresh and young';
+  const sourcePath = path.resolve(__dirname, './files/fresh.txt');
+
+  try {
+    const stat = await fs.stat(sourcePath);
+  
+    if (stat.isFile()) {
+      const err = new Error('FS operation failed');
+      err.code = 'FS_ERR';
+      throw err;
+    } else {
+      await fs.writeFile(sourcePath, data);
+    }
+  } catch (err) {
+    if (err.code === 'FS_ERR') {
+      throw err;
+    }
+
+    await fs.writeFile(sourcePath, data);
+  }  
 };
