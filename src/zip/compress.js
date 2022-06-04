f6d644c0b3998a1a461d45ec76b5ab04c0ba44a7
imychko <imychko@lumentouch.com>
2022-06-04 18:37:24 +0300

    feat: implement zip unzip functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,25 @@
+import { createReadStream, createWriteStream } from 'fs';
+import path from 'path';
+import { __dirname } from '../path.module.js';
+
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+
+const sourceFilePath = path.resolve(__dirname, './zip/files/fileToCompress.txt');
+const destinationFilePath = path.resolve(__dirname, './zip/files/archive.gz');
+
 export const compress = async () => {
-    // Write your code here 
+  const gzip = createGzip();
+  const source = createReadStream(sourceFilePath);
+  const destination = createWriteStream(destinationFilePath);
+
+  return new Promise((resolve) => {
+    resolve(
+      pipeline(source, gzip, destination, (err) => {
+        if (err) {
+          console.error('An error occurred:', err);
+        }
+      })
+    );
+  });
 };
