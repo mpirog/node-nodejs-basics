a8ed89feaf9076c65162f5b49df1af896476e32b
imychko <imychko@lumentouch.com>
2022-06-04 15:42:37 +0300

    feat: implement function that writes data

--- src/streams/index.js
+++ src/streams/index.js
@@ -1,6 +1,11 @@
 import process from 'process';
 import { read } from "./read.js";
+import { write } from "./write.js";
 
 if (process.argv.includes('--read')) {
   await read();
+}
+
+if (process.argv.includes('--write')) {
+  await write();
 }
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import { stdout as output, stdin as input } from 'process';
+
+import readline from 'readline';
+
+import { __dirname } from '../path.module.js';
+
+const sourceFilePath = path.resolve(__dirname, './streams/files/fileToWrite.txt');
+
 export const write = async () => {
-    // Write your code here 
+  const writeStream = fs.createWriteStream(sourceFilePath);
+
+  const rl = readline.createInterface({ input, output });
+
+  return new Promise((resolve) => {
+    rl.on('line', (input) => {
+      resolve(writeStream.write(`${input}\n`));
+    });
+  });
 };
