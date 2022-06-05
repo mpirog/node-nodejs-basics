6023a952b67fabacca311a154479483fe266f69b
imychko <imychko@lumentouch.com>
2022-06-05 13:19:48 +0300

    feat: implement functions that creates number of worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,48 @@
+import { Worker } from "worker_threads";
+import { cpus } from "os";
+
+import { __dirname } from "../path.module.js";
+import path from 'path';
+
+const sourceFilePath = path.resolve(__dirname, './wt/worker.js');
+
+const runWorker = (workerData) => {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(sourceFilePath, { workerData });
+
+    worker.on('message', (data) => {
+      resolve({ data, status: "resolved" });
+    });
+
+    worker.on('error', () => {
+      resolve({ data: null, status: "error" });
+    });
+    
+    worker.on('exit', (code) => {
+      if (code !== 0)
+        reject(new Error(`Worker stopped with exit code ${code}`));
+    });
+  });
+};
+
 export const performCalculations = async () => {
-    // Write your code here
+  const workers = [];
+
+  const number = cpus().length + 10;
+
+  let errNumber = Math.ceil(Math.random(number) * number);
+  errNumber = errNumber < 10 ? errNumber + 10 : errNumber; 
+  
+  for (let i = 0; i < cpus().length; i++) {
+    let workerData = {
+      number: (i + 1) + 10,
+      errNumber
+    };
+
+    workers.push(runWorker(workerData));
+  }
+  
+  Promise.all(workers).then((value) => {
+    console.log(value);
+  });
 };
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,14 @@
+import { parentPort, workerData } from 'worker_threads';
+
 // n should be received from main thread
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  if (workerData.number === workerData.errNumber) {
+    throw new Error('Error worker!');
+  }
+
+  return nthFibonacci(workerData.number);
 };
+
+parentPort.postMessage(sendResult());
