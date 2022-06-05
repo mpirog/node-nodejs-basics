8274f40f058b09fe7b787aa29bb775eb1598416a
imychko <imychko@lumentouch.com>
2022-06-05 23:29:37 +0300

    feat: implement function spawnChildProcess

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,14 @@
+import { stdout as output, stdin as input } from 'process';
+import { spawn } from 'child_process';
+import { __dirname, __filename } from "../path.module.js";
+import path from 'path';
+
+const sourceFilePath = path.resolve(__dirname, './cp/files/script.js');
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const ls = spawn('node.exe', [sourceFilePath, ...args])
+
+  ls.stdout.pipe(output);
+  
+  input.pipe(ls.stdin);
 };
---
+++ src/cp/index.js
@@ -0,0 +1,3 @@
+import { spawnChildProcess } from "./cp.js";
+
+await spawnChildProcess(process.argv.slice(2));
